930e6699fded85d4875c71eed56cab3986d75b13
Dirin Arrayales <arrayalesdirin@gmail.com>
2022-10-20 13:28:33 -0700

    problem with css

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ window.onload = function () {
     // Play
     play = function () {
       categories = [
-          ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united"],
+          ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-united", "newcastle-united"],
           ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
           ["manchester", "milan", "madrid", "amsterdam", "prague"]
       ];
@@ -211,7 +211,7 @@ window.onload = function () {
       hint.onclick = function() {
   
         hints = [
-          ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
+          ["Based in Merseyside", "Based in Merseyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "Old Trafford", "Gazza's first club"],
           ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
           ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]
       ];
